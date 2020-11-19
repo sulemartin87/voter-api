8d567f7be72c4f9feedc0f82b07fd471f5d13ad6
Martin Suleman <sulemartin87@gmail.com>
2020-11-19 22:20:34 +0200

    configuring server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://0.0.0.0:8081"
 };
 
 app.use(cors(corsOptions));
@@ -38,7 +38,8 @@ app.get("/", (req, res) => {
 require("./app/routes/vote.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+const PORT = process.argv[3] || 8080;
+const HOST = process.argv[2] || '0.0.0.0';
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on port ${HOST} ${PORT}.`);
 });
